4a9ff15fc8496138edd51c5d454f6497d0a9b488
JosephTheExperiment <yousef.hisham444@proton.me>
2025-04-26 14:21:17 +0300

    Unstable pkgs (: part 3

--- configuration.nix
+++ configuration.nix
@@ -12,9 +12,9 @@
 
   nixpkgs = {
     config = {
-      allowUnfree = true;
       packageOverrides = pkgs: {
-      unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {};
+        unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {};
+      };
     };
   };
 
