8521c5b954500200e4678f5eb5d47e3fe3ffa955
JosephTheExperiment <yousef.hisham444@proton.me>
2025-04-29 16:43:27 +0300

    Git config

--- configuration.nix
+++ configuration.nix
@@ -5,140 +5,141 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    ./modules/monitor.nix
-    ./hardware-configuration.nix
-  ];
-
-  nixpkgs = {
-    config = {
-      packageOverrides = pkgs: {
-        unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {};
-      };
-    };
-  };
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Africa/Cairo";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  environment.variables = {
-    GCM_CREDENTIAL_STORE = "secretservice";
-  };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.videoDrivers = [ "ati" "amdgpu" ];
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.gdm.wayland = false;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.joseph = {
-    isNormalUser = true;
-    description = "Joseph";
-    extraGroups = [ "wheel" "networkmanager" ];
-    packages = with pkgs; [
-      unstable.zed-editor
-      inkscape
-      obsidian
-      rustc
-      cargo
-      rust-analyzer
-      cmake
-      conan
-      vcpkg
-      gcc
-      gnumake
-      clang-tools
+    imports = [
+        ./modules/monitor.nix
+        ./hardware-configuration.nix
     ];
-  };
 
-  # Enable the Flakes feature and the accompanying new nix command-line tool
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    nixpkgs = {
+        config = {
+            packageOverrides = pkgs: {
+                unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {};
+            };
+        };
+    };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+    # Bootloader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
 
-  environment.systemPackages = with pkgs; [
-    appimage-run
-    brave
-    toybox
-    gitFull
-    git-credential-manager
-    git-credential-oauth
-  ];
+    networking.hostName = "nixos"; # Define your hostname.
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-  };
+    # Enable networking
+    networking.networkmanager.enable = true;
 
-  fonts.packages = with pkgs; [
-    fira-code
-    fira-code-symbols
-  ];
+    # Set your time zone.
+    time.timeZone = "Africa/Cairo";
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = "en_US.UTF-8";
+        LC_IDENTIFICATION = "en_US.UTF-8";
+        LC_MEASUREMENT = "en_US.UTF-8";
+        LC_MONETARY = "en_US.UTF-8";
+        LC_NAME = "en_US.UTF-8";
+        LC_NUMERIC = "en_US.UTF-8";
+        LC_PAPER = "en_US.UTF-8";
+        LC_TELEPHONE = "en_US.UTF-8";
+        LC_TIME = "en_US.UTF-8";
+    };
+
+    # Enable the X11 windowing system.
+    services.xserver.enable = true;
+    services.xserver.videoDrivers = [ "ati" "amdgpu" ];
+
+    # Enable the GNOME Desktop Environment.
+    services.xserver.displayManager.gdm.enable = true;
+    services.xserver.desktopManager.gnome.enable = true;
+    services.xserver.displayManager.gdm.wayland = false;
+
+    # Configure keymap in X11
+    services.xserver.xkb = {
+        layout = "us";
+        variant = "";
+    };
+
+    # Enable sound with pipewire.
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        # If you want to use JACK applications, uncomment this
+        #jack.enable = true;
+
+        # use the example session manager (no others are packaged yet so this is enabled by default,
+        # no need to redefine it in your config for now)
+        #media-session.enable = true;
+    };
+
+    # Enable touchpad support (enabled default in most desktopManager).
+    # services.xserver.libinput.enable = true;
+
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.joseph = {
+        isNormalUser = true;
+        description = "Joseph";
+        extraGroups = [ "wheel" "networkmanager" ];
+        packages = with pkgs; [
+            unstable.zed-editor
+            inkscape
+            obsidian
+            rustc
+            cargo
+            rust-analyzer
+            cmake
+            conan
+            vcpkg
+            gcc
+            gnumake
+            clang-tools
+        ];
+    };
+
+    # Enable the Flakes feature and the accompanying new nix command-line tool
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    environment.systemPackages = with pkgs; [
+        appimage-run
+        brave
+        toybox
+        git-credential-manager
+        neovim
+    ];
+
+    programs.git = {
+        enable = true;
+        package = pkgs.gitFull;
+        config = {
+            user = {
+                name = "JosephTheExperiment";
+	            email = "yousef.hisham444@proton.me";
+            };
+            credential = {
+	            helper = "manager";
+				store = "libsecret";
+	        };
+        };
+    };
+
+    fonts.packages = with pkgs; [
+        fira-code
+        fira-code-symbols
+    ];
+
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "24.11"; # Did you read the comment?
 }
