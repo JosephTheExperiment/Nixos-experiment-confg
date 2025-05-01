a65b8bd440da89e71d3a239b747269474c09bfa9
JosephTheExperiment <yousef.hisham444@proton.me>
2025-05-01 16:22:00 +0300

    First use of flakes 7

--- flake.nix
+++ flake.nix
@@ -4,8 +4,8 @@
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, unstable, ... }@inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+  outputs = { stable-pkgs, unstable, ... }@inputs: {
+    nixosConfigurations.nixos = stable-pkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
