82e7c8c31afa1fe53e942e65c93e1ecc80199fa4
JosephTheExperiment <yousef.hisham444@proton.me>
2025-05-06 02:37:11 +0300

    Update bash script

--- rebuild.sh
+++ rebuild.sh
@@ -1,11 +1,15 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p bash
+set -e
 
 git add .
 echo "Git commit massage:"
 read commitMassage
+
+echo "\n"
 git commit -m "$commitMassage"
 
+echo "\n"
 echo "Do you want to?"
 echo "1: Nixos rebuild switch"
 echo "2: Home manager switch"
@@ -13,6 +17,7 @@ echo "3: 1 and 2"
 echo "4: Nothing"
 read actionNumber
 
+echo "\n"
 if [ $actionNumber -eq 1 ]; then 
   sudo nixos-rebuild switch --flake /home/joseph/Nixos-config/
 elif [ $actionNumber -eq 2 ]; then 
