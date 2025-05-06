633e4e378502905b81b38d5e3ccf92108d702202
JosephTheExperiment <yousef.hisham444@proton.me>
2025-05-06 12:37:37 +0300

    Update protonup 2

--- home.nix
+++ home.nix
@@ -39,7 +39,7 @@
 
   home.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATHS =
-      "/${HOME}/.steam/root/compatibilitytools.d";
+      "\${HOME}/.steam/root/compatibilitytools.d";
   };
 
   # Git config
