fa36dc92c8070bb4df24e5cfbe0088dac0bc6064
JosephTheExperiment <yousef.hisham444@proton.me>
2025-05-07 13:29:40 +0300

    2

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-24.11"`
+      url = "github:nix-community/nixvim/nixos-24.11"`;
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
