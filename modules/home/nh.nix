811be54d661fff240b3a007eeb59f5d37354f59d
JosephTheExperiment <yousef.hisham444@proton.me>
2025-05-08 14:44:54 +0300

    Nh try 6

--- modules/home/nh.nix
+++ modules/home/nh.nix
@@ -1,7 +1,7 @@
-{ config, pkgs-unstable, ... }: {
+{ pkgs, ... }: {
   programs.nh = {
     enable = true;
-    package = pkgs-unstable.nh;
+    package = pkgs.nh;
     flake = "/home/joseph/Nixos-config";
     clean.enable = true;
     clean.extraArgs = "--keep-since 3d --keep 3";
