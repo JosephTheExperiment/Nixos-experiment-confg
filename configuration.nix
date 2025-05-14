dd412bb0a184d604da586a84f500cb24d0d271a8
JosephTheExperiment <yousef.hisham444@proton.me>
2025-05-14 23:24:51 +0300

    .

--- configuration.nix
+++ configuration.nix
@@ -19,14 +19,7 @@
   # Set the video drivers for amd gpu's
   services.xserver = {
     enable = true;
-    displayManager.lightdm = {
-      greeters.pantheon.enable = true;
-      greeter = {
-        enable = true;
-        package = pkgs.pantheon.elementary-greeter;
-        name = "io.elementary.greeter";
-      };
-    };
+    displayManager.lightdm.enable = true;
     desktopManager.budgie.enable = true;
     videoDrivers = [ "ati" "amdgpu" ];
   };
