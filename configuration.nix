968110388464b7be3a2f86d40902eca498e040b1
JosephTheExperiment <yousef.hisham444@proton.me>
2025-05-14 10:13:01 +0300

    Fix typo in configuration.nix file

--- configuration.nix
+++ configuration.nix
@@ -21,7 +21,7 @@
     enable = true;
     displayManager.lightdm = {
       enable = true;
-      pantheon.enable = true;
+      greeters.pantheon.enable = true;
     };
     desktopManager.budgie.enable = true;
     videoDrivers = [ "ati" "amdgpu" ];
