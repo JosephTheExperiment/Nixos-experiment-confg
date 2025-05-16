30044bc9da681991751ef47e5c77a3df4a593352
JosephTheExperiment <yousef.hisham444@proton.me>
2025-05-16 21:09:06 +0300

    Added greeter module

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,1 +1,1 @@
-{ imports = [ ./monitor.nix ./../shared/gaming/nixos.nix ]; }
+{ imports = [ ./monitor.nix ./greeter.nix ./../shared/gaming/nixos.nix ]; }
---
+++ modules/nixos/greeter.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }: {
+  services.xserver.displayManager.lightdm.greeters.gtk = {
+    enable = true;
+    theme = {
+      name = "Flat-Remix-GTK-Blue-Dark";
+      package = pkgs.flat-remix-gtk;
+    };
+    iconTheme = {
+      name = "Flat-Remix-Blue-Dark";
+      package = pkgs.flat-remix-icon-theme;
+    };
+    cursorTheme = {
+      package = pkgs.vimix-cursor-theme;
+      name = "Vimix-Cursors";
+      size = 16;
+    };
+    extraConfig = ''
+      user-background = true
+    '';
+  };
+}
