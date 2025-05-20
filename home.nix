3b3f5686d15e8a039251dc4bc6f16ea1c551a9e0
JosephTheExperiment <yousef.hisham444@proton.me>
2025-05-20 22:33:53 +0300

    Added zen browser from nix-flatpak 1

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ pc, pkgs, pkgs-unstable, ... }: {
+{ pc, lib, pkgs, pkgs-unstable, ... }: {
   home.username = pc.user;
   home.homeDirectory = "/home/${pc.user}";
   home.stateVersion = "24.05"; # Don't change this.
---
+++ modules/home/flatpak.nix
@@ -0,0 +1,9 @@
+{ lib, ... }: {
+  services.flatpak = {
+    remotes = lib.mkOptionDefault [{
+      name = "flathub";
+      location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+    }];
+    packages = [ "app.zen_browser.zen" ];
+  };
+}
