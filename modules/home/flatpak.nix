bf5320b292400ada8742cc1fd4449fde84152a19
JosephTheExperiment <yousef.hisham444@proton.me>
2025-05-21 22:27:21 +0300

    Added zen

--- modules/home/flatpak.nix
+++ modules/home/flatpak.nix
@@ -1,12 +1,1 @@
-{
-  services.flatpak = {
-    remotes = [{
-      name = "flathub";
-      location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-    }];
-    packages = [{
-      appId = "app.zen_browser.zen";
-      origin = "flathub";
-    }];
-  };
-}
+{ services.flatpak = { packages = [ "app.zen_browser.zen" ]; }; }
