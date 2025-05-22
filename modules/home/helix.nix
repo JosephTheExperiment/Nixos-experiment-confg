1f7482b5043b0567ab4997c6fa70c44fd1ab6519
JosephTheExperiment <yousef.hisham444@proton.me>
2025-05-23 01:25:37 +0300

    Fixed helix module errors 2

--- modules/home/helix.nix
+++ modules/home/helix.nix
@@ -1,14 +1,12 @@
-{ pkgs, ...}: {
+{ pkgs, ... }: {
   programs.helix = {
     enable = true;
     defaultEditor = true;
-    settings = {
-      theme = "tokyonight";
-    };
+    settings = { theme = "tokyonight"; };
     languages.language = [{
       name = "nix";
       auto-format = true;
       formatter.command = "${pkgs.nixfmt-classic}/bin/nixfmt";
-    };];
+    }];
   };
 }
