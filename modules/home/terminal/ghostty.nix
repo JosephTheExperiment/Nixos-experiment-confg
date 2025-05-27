216d92672f411578aae059ad726bc38ba2d3afc5
JosephTheExperiment <yousef.hisham444@proton.me>
2025-05-27 17:27:08 +0300

    Ghostty config 1

--- modules/home/terminal/ghostty.nix
+++ modules/home/terminal/ghostty.nix
@@ -2,5 +2,10 @@
   programs.ghostty = {
     enableFishIntegration = true;
     enable = true;
+    settings = {
+      theme = "tokyonight";
+      font-size = 12;
+      font-family = "FiraCode Nerd Font Mono Med";
+    };
   };
 }
