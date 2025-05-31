ef8407f8985fbbdb2ed4a7a8e8fbae5e33806dc8
JosephTheExperiment <yousef.hisham444@proton.me>
2025-05-31 10:16:53 +0300

    Changed helix package to to evil-helix, and added so,e config to it

--- modules/home/terminal/programs/helix.nix
+++ modules/home/terminal/programs/helix.nix
@@ -1,15 +1,29 @@
 { pkgs, ... }: {
   programs.helix = {
     enable = true;
+    package = pkgs.evil-helix;
     defaultEditor = true;
     settings = {
       theme = "tokyonight";
       keys.insert = { "Win-q" = "normal_mode"; };
     };
-    languages.language = [{
-      name = "nix";
-      auto-format = true;
-      formatter.command = "${pkgs.nixfmt-classic}/bin/nixfmt";
-    }];
+    languages.language = [
+      {
+        name = "nix";
+        auto-format = true;
+      }
+      {
+        name = "rust";
+        auto-format = true;
+      }
+      {
+        name = "cpp";
+        auto-format = true;
+      }
+      {
+        name = "c";
+        auto-format = true;
+      }
+    ];
   };
 }
