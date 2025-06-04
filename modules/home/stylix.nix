1bdfaaacf7130edc03ab67cc8b1833ef2088c318
JosephTheExperiment <yousef.hisham444@proton.me>
2025-06-04 23:07:46 +0300

    Added new font and some font configuration to stylix 3

--- modules/home/stylix.nix
+++ modules/home/stylix.nix
@@ -11,14 +11,6 @@
     };
     fonts = {
       sizes.applications = 10;
-      packages = [
-        pkgs.noto-fonts
-        pkgs.noto-fonts-color-emoji
-        pkgs.nerd-fonts.fira-code
-        pkgs.nerd-fonts.jetbrains-mono
-        pkgs.nerd-fonts.symbols-only
-        pkgs.nerd-fonts.sauce-code-pro
-      ];
       serif = {
         package = pkgs.nerd-fonts.sauce-code-pro;
         name = "SauceCodePro Nerd Font";
