a85127fafd4a81f7ff92d75153a910c251cb3a28
JosephTheExperiment <yousef.hisham444@proton.me>
2025-06-05 01:50:12 +0300

    Changed font configuration to stylix 6

--- modules/home/stylix.nix
+++ modules/home/stylix.nix
@@ -10,12 +10,23 @@
       kde.enable = false;
     };
     fonts = {
-      packages = [ pkgs.nerd-fonts.sauce-code-pro pkgs.noto-fonts-emoji ];
       sizes.applications = 10;
-      serif.name = "SauceCodePro Nerd Font";
-      sansSerif.name = "SauceCodePro Nerd Font";
-      monospace.name = "SauceCodePro Nerd Font Mono";
-      emoji.name = "Noto Color Emoji";
+      serif = {
+        name = "SauceCodePro Nerd Font";
+        package = pkgs.nerd-fonts.sauce-code-pro;
+      };
+      sansSerif = {
+        name = "SauceCodePro Nerd Font";
+        package = pkgs.nerd-fonts.sauce-code-pro;
+      };
+      monospace = {
+        name = "SauceCodePro Nerd Font Mono";
+        package = pkgs.nerd-fonts.sauce-code-pro;
+      };
+      emoji = {
+        name = "Noto Color Emoji";
+        package = pkgs.noto-fonts-emoji;
+      };
     };
   };
 }
