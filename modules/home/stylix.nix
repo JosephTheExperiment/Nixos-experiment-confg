54fc011495ea03bf8d573095473d2c58f898ba9a
JosephTheExperiment <yousef.hisham444@proton.me>
2025-06-04 23:02:31 +0300

    Added new font and some font configuration to stylix 2

--- modules/home/stylix.nix
+++ modules/home/stylix.nix
@@ -9,6 +9,32 @@
       gtk.enable = false;
       kde.enable = false;
     };
-    fonts.sizes.applications = 10;
+    fonts = {
+      sizes.applications = 10;
+      packages = [
+        pkgs.noto-fonts
+        pkgs.noto-fonts-color-emoji
+        pkgs.nerd-fonts.fira-code
+        pkgs.nerd-fonts.jetbrains-mono
+        pkgs.nerd-fonts.symbols-only
+        pkgs.nerd-fonts.sauce-code-pro
+      ];
+      serif = {
+        package = pkgs.nerd-fonts.sauce-code-pro;
+        name = "SauceCodePro Nerd Font";
+      };
+      sansSerif = {
+        package = pkgs.nerd-fonts.sauce-code-pro;
+        name = "SauceCodePro Nerd Font";
+      };
+      monospace = {
+        package = pkgs.nerd-fonts.sauce-code-pro;
+        name = "SauceCodePro Nerd Font Mono";
+      };
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
   };
 }
--- modules/nixos/fonts.nix
+++ modules/nixos/fonts.nix
@@ -1,20 +1,10 @@
-{ pkgs, ... }: {
+{
   fonts = {
-    enableDefaultPackages = true;
     fontDir.enable = true;
-    packages = [
-      pkgs.noto-fonts
-      pkgs.noto-fonts-color-emoji
-      pkgs.nerd-fonts.fira-code
-      pkgs.nerd-fonts.jetbrains-mono
-      pkgs.nerd-fonts.symbols-only
-      pkgs.nerd-fonts.sauce-code-pro
-    ];
     fontconfig = {
       enable = true;
       antialias = true;
       cache32Bit = true;
-      defaultFonts = { emoji = [ "Noto Color Emoji" ]; };
       hinting.enable = true;
       hinting.style = "medium";
     };
