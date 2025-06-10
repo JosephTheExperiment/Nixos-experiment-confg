f59d59198455a8df47e51ce02b07e4be7882a94c
JosephTheExperiment <yousef.hisham444@proton.me>
2025-06-11 02:33:59 +0300

    Added ppsspp and pcsx2 emulators

--- modules/home/gaming.nix
+++ modules/home/gaming.nix
@@ -1,5 +1,6 @@
 { pc, pkgs, ... }: {
-  home.packages = [ pkgs.mangohud pkgs.protonup-ng pkgs.heroic ];
+  home.packages =
+    [ pkgs.mangohud pkgs.protonup-ng pkgs.heroic pkgs.ppsspp pkgs.pcsx2 ];
 
   home.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATHS =
