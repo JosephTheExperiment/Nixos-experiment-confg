2e7c13129c341170e0c4dc81da06e12ee80b20ba
JosephTheExperiment <yousef.hisham444@proton.me>
2025-06-14 01:21:39 +0300

    Deleted all emulators

--- modules/home/gaming.nix
+++ modules/home/gaming.nix
@@ -1,12 +1,5 @@
 { pc, pkgs, ... }: {
-  home.packages = [
-    pkgs.mangohud
-    pkgs.protonup-ng
-    pkgs.heroic
-    pkgs.ppsspp
-    pkgs.pcsx2
-    pkgs.duckstation
-  ];
+  home.packages = [ pkgs.mangohud pkgs.protonup-ng pkgs.heroic ];
 
   home.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATHS =
