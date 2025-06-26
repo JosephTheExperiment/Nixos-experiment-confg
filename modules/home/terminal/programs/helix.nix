ec4dac296aa74204b0da551184494a91af2cb489
JosephTheExperiment <yousef.hisham444@proton.me>
2025-06-26 10:53:13 +0300

    Added cllipy to rust in helix

--- modules/home/terminal/programs/helix.nix
+++ modules/home/terminal/programs/helix.nix
@@ -4,23 +4,26 @@
     package = pkgs.evil-helix;
     defaultEditor = true;
     settings = { keys.insert = { "A-q" = "normal_mode"; }; };
-    languages.language = [
-      {
-        name = "nix";
-        auto-format = true;
-      }
-      {
-        name = "rust";
-        auto-format = true;
-      }
-      {
-        name = "cpp";
-        auto-format = true;
-      }
-      {
-        name = "c";
-        auto-format = true;
-      }
-    ];
+    languages = {
+      language-server.rust-analyzer.config = { check.command = "clippy"; };
+      language = [
+        {
+          name = "nix";
+          auto-format = true;
+        }
+        {
+          name = "rust";
+          auto-format = true;
+        }
+        {
+          name = "cpp";
+          auto-format = true;
+        }
+        {
+          name = "c";
+          auto-format = true;
+        }
+      ];
+    };
   };
 }
