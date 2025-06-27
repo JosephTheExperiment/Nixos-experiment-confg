1cf86eb733df85d657b0a8bb17ae5c2070c2108f
JosephTheExperiment <yousef.hisham444@proton.me>
2025-06-27 08:07:24 +0300

    Added more to helix 2

--- modules/home/terminal/programs/helix.nix
+++ modules/home/terminal/programs/helix.nix
@@ -25,13 +25,13 @@
       };
       keys = {
         insert = {
-          C.n = "normal_mode";
-          C.j = "move_visual_line_down";
-          C.k = "move_visual_line_up";
-          C.h = "move_char_left";
-          C.l = "move_char_right";
+          C-n = "normal_mode";
+          C-j = "move_visual_line_down";
+          C-k = "move_visual_line_up";
+          C-h = "move_char_left";
+          C-l = "move_char_right";
         };
-        select = { "C.n" = "normal_mode"; };
+        select = { "C-n" = "normal_mode"; };
       };
     };
     languages = {
